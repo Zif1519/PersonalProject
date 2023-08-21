3d84435aae5da93426435f4a271bc59b60637070
Sooyong Choi <forhamgame@gmail.com>
2023-08-21 20:14:41 +0900

    [UPDATE] Added Displayer class and other classes

--- RPG_Game/Program.cs
+++ RPG_Game/Program.cs
@@ -3,13 +3,18 @@ using System.Text;
 
 namespace RPG_Game
 {
-    public enum ItemType { Weapon = 0, SubWeapon, Halmet, Armor, Gloves, Boots, Ring, Amulet, Potion, Food}
-
+    public enum GAME_STATUS { }
+    public enum ITEM_TYPE { Weapon = 0, SubWeapon, Halmet, Armor, Gloves, Boots, Ring, Amulet, Potion, Food, }
+    public enum SELECT_TYPE { Place = 0, Status, Inventory, Item, Quest,}
     internal class Program
     {
         static void Main(string[] args)
         {
-            SetSenario();
+            Displayer displayer = new Displayer();
+            Inventory inventory = new Inventory();
+            Dictionary<String, ISelectable> myData = new Dictionary<string, ISelectable>();
+            
+            SetSenario(ref myData);
             if (true)
             {
                 //1.게임 시작 화면
@@ -109,41 +114,100 @@ namespace RPG_Game
                 //    -아이템이 장착되었다면 1.상태보기 에 정보가 반영되어야 합니다.
                 //        -정보 반영 예제
             }
-        }
 
-        static void SetSenario()
-        {
-            // 캐릭터 생성 및 초기화 등
+            while (true)
+            {
 
-            // 스테이지 생성 및 초기화 등
+            }
 
-            // 첫 화면 로드
-        }
-
-        static void DisplayView()
-        {
             
         }
-
-        static void LoadStageView()
+        static void SetSenario(ref Dictionary<String, ISelectable> data)
         {
-            StringBuilder title = stage.Name;
-            StringBuilder description = stage.Description;
-            Stage[] selections = stage.Selections;
+            data.Add() 
+        }
 
+        static int GetValidInput(int min, int max)
+        {
+            while (true)
+            {
+                Console.Write($"원하시는 행동의 번호를 입력해 주세요. : ");
+                string answer = Console.ReadLine();
+                bool isParseSuccess = int.TryParse(answer, out int result)
+                if (isParseSuccess)
+                {
+                    if(result >= min && result <= max) return result;
+                }
+            }
+        }
+    }
 
+    public interface ISelectable
+    {
+        string Name { get; }
+        string Description { get; }
+        public SELECT_TYPE SelectType { get; }
+        public List<ISelectable> Selections { get; }
+        public void AddSelection(ISelectable selection)
+        {
+            Selections.Add(selection);
+        }
+    }
+
+    public class Displayer
+    {
+        StringBuilder _title;
+        StringBuilder _description;
+        StringBuilder _selections;
+
+        public void RefreshDisplay()
+        {
             // 콘솔창 지우기
             Console.Clear();
 
             // 콘솔에 스테이지 Title을 표시하기
             Console.WriteLine("########################################");
-            Console.WriteLine($"          {title}          ");
-            Console.WriteLine("########################################\n\n\n");
+            Console.WriteLine($"          {_title}          ");
+            Console.WriteLine("########################################\n\n");
 
             // 스테이지 설명이 있는 경우 출력.
-            if (description != null)
+            if (stageData.Description != null && stageData.Description != "")
             {
                 Console.WriteLine(description);
+            }
+            else { Console.WriteLine("\n\t\t  ...\t\t\n"); }
+
+            // 선택지 출력
+            Console.WriteLine(_Selection);
+            Console.WriteLine("\n\n");
+        }
+        public void UpdatePlaceData(PlaceData placeData)
+        {
+            // PlaceData가 들어오면 모든 정보 초기화 
+            _title.Clear();
+            _description.Clear();
+            _selections.Clear();
+            _selectCount = 0;
+
+            // 재설정
+            _title.Append(placeData.Name);
+            _description.Append(placeData.Description);
+            if{ (placeData.Selections.Count <= 0) return; }
+            else { _selectCount = placeData.Selections.Count; }
+            for(int i =0; i< placeData.Selections.Count; i++)
+            {
+                _selections.Append($"{i + 1} : ");
+                switch (placeData.Selections[i].SelectType)
+                {
+                    case : SELECT_TYPE.Place
+                        _selections.Append($"[{placeData.Selections[i].Name}](으)로 이동.");
+                        break;
+                        
+                        // 퀘스트나 아이템의 경우 추가로 구현
+
+                    default:
+                        break;
+                }
             }
 
             // 플레이어의 선택지인 Selections에 번호를 붙여서 표시하기
@@ -151,67 +215,101 @@ namespace RPG_Game
             {
                 Console.WriteLine($" {i + 1} : [{selections[i].Name}](으)로 가기");
             }
-
             Console.WriteLine("\n\n");
 
-            // 플레이어의 행동을 입력받는다. 행동의 번호 외의 숫자가 입력된 경우에는 다시 입력을 받는다.
-            bool isCorrectInput = false;
-            while (!isCorrectInput)
-            {
-                try
-                {
-                    Console.Write($"원하시는 행동의 번호를 입력해 주세요. (1~{selections.Length}) : ");
-                    int answer = int.Parse(Console.ReadLine());
-                    if (answer < 1 || answer > selections.Length)
-                    {
-                        LoadStage(stage.Selections[answer - 1]);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"입력이 올바르지 않습니다. Exception : {e}");
-                }
-            }
+            RefreshDisplay();
         }
-        
-        static void LoadInventoryView()
+        public void UpdateQuestData(QuestData QuestData)
         {
-
-        }
-
-        static void LoadStatusView()
-        {
-
+            RefreshDisplay();
         }
     }
-
-    public interface ISelectable
+    public class PlaceData : ISelectable
     {
-        StringBuilder Name { get; }
-        void OnSelected();
-    }
-
-    public abstract class Stage : ISelectable
-    {
-        public StringBuilder Name { get;}
-        public StringBuilder Description { get;}
-        public List<ISelectable> Selections { get;}
-        public Stage(StringBuilder name, StringBuilder description)
+        public string Name { get; }
+        public string Description { get; }
+        public SELECT_TYPE SelectType { get; } = SELECT_TYPE.Place;
+        public List<ISelectable> Selections { get; }
+        public PlaceData(string name, string description)
         {
             Name = name;
             Description = description;
             Selections = new List<ISelectable>();
         }
-
         public void AddSelection(ISelectable selection)
         {
             Selections.Add(selection);
         }
-        public void OnSelected()
+    }
+    public class QuestData
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public SELECT_TYPE SelectType { get; } = SELECT_TYPE.Quest;
+        public List<ISelectable> Selections { get; }
+        public Inventory(String name, string description)
         {
-           
+            Name = name;
+            Description = description;
+            Selections = new List<ISelectable>();
+        }
+        public void AddSelection(ISelectable selection)
+        {
+            Selections.Add(selection);
         }
     }
+    public class InventoryData : ISelectable
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public SELECT_TYPE SelectType { get; } = SELECT_TYPE.Inventory;
+        public List<ISelectable> Selections { get; }
+        public Inventory(String name, string description)
+        {
+            Name = name;
+            Description = description;
+            Selections = new List<ISelectable>();
+        }
+        public void AddSelection(ISelectable selection)
+        {
+            Selections.Add(selection);
+        }
+    }
+    public class StatusData
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public SELECT_TYPE SelectType { get; } = SELECT_TYPE.Status;
+        public List<ISelectable> Selections { get; }
+        public Inventory(String name, string description)
+        {
+            Name = name;
+            Description = description;
+            Selections = new List<ISelectable>();
+        }
+        public void AddSelection(ISelectable selection)
+        {
+            Selections.Add(selection);
+        }
+    }
+    public class ItemData : ISelectable
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public SELECT_TYPE SelectType { get; } = SELECT_TYPE.Item;
+        public List<ISelectable> Selections { get; }
+        public Inventory(String name, string description)
+        {
+            Name = name;
+            Description = description;
+            Selections = new List<ISelectable>();
+        }
+        public void AddSelection(ISelectable selection)
+        {
+            Selections.Add(selection);
+        }
+    }
+
 
     public class Character
     {
@@ -235,13 +333,5 @@ namespace RPG_Game
             CurHp = maxhp;
             Gold = gold;
         }
-    }
-
-    public class Item : ISelectable
-    {
-        public StringBuilder Name { get;}
-        public StringBuilder Description { get;} 
-        public ItemType Type { get; }
-        public int Price { get;}
     }
 }
